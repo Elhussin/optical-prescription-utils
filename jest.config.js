e519ababa38ce5b362c1282f39707a1c2d8ea675
Elhussin <hasin3112@gmail.com>
2025-10-29 00:16:13 +0300

    updat packge file

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,3 @@
-// // jest.config.js
-
-// /** @type {import('ts-jest').JestConfigWithTsJest} */
-// export default {
-//   // استخدام ts-jest كمعالج لملفات .ts
-//   preset: 'ts-jest', 
-  
-//   // تحديد بيئة العمل كـ nodejs
-//   testEnvironment: 'node', 
-  
-//   // أنماط الملفات التي يجب اختبارها
-//   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'], 
-  
-//   // معالجة وحدات ES (ضروري لوجود "type": "module")
-//   moduleFileExtensions: ['js', 'ts'],
-  
-//   // تحديد مسار ملفات الاختبار
-//   roots: ['<rootDir>/src', '<rootDir>/test'],
-// };
-
-// jest.config.js (باستخدام SWC)
-
 export default {
   // استخدام swc-jest كمعالج لملفات .ts
   transform: {
